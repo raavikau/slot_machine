2b23235e1281044fe0ecaeb599e29b8e15de0603
Raavi Kaur <raavi211299@gmail.com>
2024-08-21 22:13:31 -0500

    add a skelton code for slot machine

---
+++ main.py
@@ -0,0 +1,24 @@
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def deposit():
+    pass
+def get_number_of_lines():
+    pass
+def get_bet():
+    pass
+def spin(balance):
+    pass
+def main():
+    pass
